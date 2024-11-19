5007399450dc7b6dfceff63b95f848451fec31ea
Hozan Kousa <hoz.k.95@gmail.com>
2024-11-19 14:46:44 +0100

    commit -a
    "commit build folder"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Houzan Kousa is s
+          Houzan Kousa is live now!
         </p>
         <a
           className="App-link"
