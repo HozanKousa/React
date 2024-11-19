1581fd4955f237c45d75854209b4c0409de4d967
Hozan Kousa <hoz.k.95@gmail.com>
2024-11-19 14:34:41 +0100

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Houzan Kousa is Live
+          Houzan Kousa is s
         </p>
         <a
           className="App-link"
